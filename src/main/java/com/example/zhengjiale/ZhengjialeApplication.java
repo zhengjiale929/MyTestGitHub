d7068ca76b1060a8fa7268b49bf8c8219f07fbf6
zhengjiale-ds <zhengjiale-ds@gome.com.cn>
2019-05-30 11:06:13 +0800

    测试

---
+++ src/main/java/com/example/zhengjiale/ZhengjialeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhengjiale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhengjialeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhengjialeApplication.class, args);
+    }
+
+}
